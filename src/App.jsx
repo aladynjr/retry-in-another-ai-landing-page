210e67e9c0abe53166dfdb8d2fcf77f4883d4281
unknown <aladynjr@gmail.com>
2024-11-02 21:39:47 +0100

    add firebase hosting

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
 import { Button } from "../components/ui/button";
 import { ArrowRight, MessageSquare, Chrome } from "lucide-react"
 import { motion } from "framer-motion"
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAwdLYVCr6BPjBSnpba1-g5dDCK8URlxJE",
+  authDomain: "retry-in-another-ai-landing.firebaseapp.com",
+  projectId: "retry-in-another-ai-landing",
+  storageBucket: "retry-in-another-ai-landing.firebasestorage.app",
+  messagingSenderId: "1014600876594",
+  appId: "1:1014600876594:web:a804663fcc77aa54c2722b",
+  measurementId: "G-STMWDFQSZT"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 export const SpeakerIcon = () => (
   <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="icon-md-heavy">
     <path fillRule="evenodd" clipRule="evenodd" d="M11 4.9099C11 4.47485 10.4828 4.24734 10.1621 4.54132L6.67572 7.7372C6.49129 7.90626 6.25019 8.00005 6 8.00005H4C3.44772 8.00005 3 8.44776 3 9.00005V15C3 15.5523 3.44772 16 4 16H6C6.25019 16 6.49129 16.0938 6.67572 16.2629L10.1621 19.4588C10.4828 19.7527 11 19.5252 11 19.0902V4.9099ZM8.81069 3.06701C10.4142 1.59714 13 2.73463 13 4.9099V19.0902C13 21.2655 10.4142 22.403 8.81069 20.9331L5.61102 18H4C2.34315 18 1 16.6569 1 15V9.00005C1 7.34319 2.34315 6.00005 4 6.00005H5.61102L8.81069 3.06701ZM20.3166 6.35665C20.8019 6.09313 21.409 6.27296 21.6725 6.75833C22.5191 8.3176 22.9996 10.1042 22.9996 12.0001C22.9996 13.8507 22.5418 15.5974 21.7323 17.1302C21.4744 17.6185 20.8695 17.8054 20.3811 17.5475C19.8927 17.2896 19.7059 16.6846 19.9638 16.1962C20.6249 14.9444 20.9996 13.5175 20.9996 12.0001C20.9996 10.4458 20.6064 8.98627 19.9149 7.71262C19.6514 7.22726 19.8312 6.62017 20.3166 6.35665Z" fill="currentColor" />
